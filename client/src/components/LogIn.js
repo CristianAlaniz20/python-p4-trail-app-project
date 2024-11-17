a80b807800773ec65ddd0d40b75b8930d8bd70d9
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-16 18:18:22 -0800

    add button and change for attribute to htmlFor attributte for LogIn and SignUp components

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -3,17 +3,19 @@ import React from "react";
 function LogIn() {
     return (
         <form>
-            <label for='username'>Username: </label>
+            <label htmlFor='username'>Username: </label>
             <input
             id='username' 
             name='username' 
             />
             <br />
-            <label for='password'>Password: </label>
+            <label htmlFor='password'>Password: </label>
             <input
             id='password'
             name='password'
             />
+            <br />
+            <button>Log In</button>
       </form>
     )
 }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -3,27 +3,29 @@ import React from "react";
 function SignUp() {
     return (
         <form>
-            <label for='username'>Username: </label>
+            <label htmlFor='username'>Username: </label>
             <input
             id='username' 
             name='username' 
             />
             <br />
-            <label for='password'>Password: </label>
+            <label htmlFor='password'>Password: </label>
             <input
             id='password'
             name='password'
             />
-            <label for='profile-pic'>Profile Pic Image URL: </label>
+            <label htmlFor='profile-pic'>Profile Pic Image URL: </label>
             <input
             id='profile-pic'
             name='profile-pic'
             />
-            <label for='bio'>Bio: </label>
+            <label htmlFor='bio'>Bio: </label>
             <input
             id='bio'
             name='bio'
             />
+            <br />
+            <button>Sign Up</button>
       </form>
     )
 }
