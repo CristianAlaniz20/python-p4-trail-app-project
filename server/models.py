6ad266a1a0bcc2c2cf8cee388e38873c332b8ceb
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-17 14:19:07 -0800

    create initial models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,27 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+    
+    pass
+
+class Trail(db.Model, SerializerMixin):
+    __tablename__ = "trails"
+
+    pass
+
+class UserTrail(db.Model, SerializerMixin):
+    __tablename__ = "user_trails"
+
+    pass
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    pass
