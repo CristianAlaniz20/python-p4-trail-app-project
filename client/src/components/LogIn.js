cf33013ae2ef6538e66457641d08a73b9ac421e8
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-16 18:38:07 -0800

    add form title and button for LogIn and SignUp components

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,21 +2,24 @@ import React from "react";
 
 function LogIn() {
     return (
-        <form>
-            <label htmlFor='username'>Username: </label>
-            <input
-            id='username' 
-            name='username' 
-            />
-            <br />
-            <label htmlFor='password'>Password: </label>
-            <input
-            id='password'
-            name='password'
-            />
-            <br />
-            <button>Log In</button>
-      </form>
+        <>
+            <h2>Log In</h2>
+            <form>
+                <label htmlFor='username'>Username: </label>
+                <input
+                id='username' 
+                name='username' 
+                />
+                <br />
+                <label htmlFor='password'>Password: </label>
+                <input
+                id='password'
+                name='password'
+                />
+                <br />
+                <button>Log In</button>
+        </form>
+      </>
     )
 }
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,31 +2,34 @@ import React from "react";
 
 function SignUp() {
     return (
-        <form>
-            <label htmlFor='username'>Username: </label>
-            <input
-            id='username' 
-            name='username' 
-            />
-            <br />
-            <label htmlFor='password'>Password: </label>
-            <input
-            id='password'
-            name='password'
-            />
-            <label htmlFor='profile-pic'>Profile Pic Image URL: </label>
-            <input
-            id='profile-pic'
-            name='profile-pic'
-            />
-            <label htmlFor='bio'>Bio: </label>
-            <input
-            id='bio'
-            name='bio'
-            />
-            <br />
-            <button>Sign Up</button>
-      </form>
+        <>
+            <h2>Sign Up</h2>
+            <form>
+                <label htmlFor='username'>Username: </label>
+                <input
+                id='username' 
+                name='username' 
+                />
+                <br />
+                <label htmlFor='password'>Password: </label>
+                <input
+                id='password'
+                name='password'
+                />
+                <label htmlFor='profile-pic'>Profile Pic Image URL: </label>
+                <input
+                id='profile-pic'
+                name='profile-pic'
+                />
+                <label htmlFor='bio'>Bio: </label>
+                <input
+                id='bio'
+                name='bio'
+                />
+                <br />
+                <button>Sign Up</button>
+        </form>
+      </>
     )
 }
 
