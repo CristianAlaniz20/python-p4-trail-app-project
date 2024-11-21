94e15e38bddd01f033a1f89f17d34df1309db37e
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-21 10:26:32 -0800

    edit SignUpForm component to include formik and yup

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,36 +1,75 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 function SignUpForm() {
+    const formSchema = yup.object().shape({
+        username: yup.string().required("Must enter a username."),
+        password: yup.string().required("Must enter a password")
+    })
+
+    const formik = useFormik({
+        initialValues : {
+            username: "",
+            password: "",
+            profileImage: "",
+            bio: ""
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            console.log(values)
+            fetch("signup", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(values, null, 2),
+            })
+            .then((res) => {
+              if (res.status === 201) {
+                console.log("Sucessfully signed up!")
+              }
+            })
+          },
+        })
+    
     return (
         <>
-            <h2>Sign Up</h2>
-            <form>
+            <h2>User Sign Up Form</h2>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='username'>Username: </label>
                 <input
                 id='username' 
-                name='username' 
+                name='username'
+                onChange={formik.handleChange}
+                value={formik.values.username}
                 />
                 <br />
                 <label htmlFor='password'>Password: </label>
                 <input
                 id='password'
                 name='password'
+                onChange={formik.handleChange}
+                value={formik.values.password}
                 />
                 <br />
-                <label htmlFor='profile-pic'>Profile Image: </label>
+                <label htmlFor='profileImage'>Profile Image URL: </label>
                 <input
-                placeholder="image url"
-                id='profile-pic'
-                name='profile-pic'
+                id='profileImage'
+                name='profileImage' 
+                onChange={formik.handleChange}
+                value={formik.values.profileImage}
                 />
                 <br />
                 <label htmlFor='bio'>Bio: </label>
                 <input
                 id='bio'
                 name='bio'
+                onChange={formik.handleChange}
+                value={formik.values.bio}
                 />
                 <br />
-                <button>Sign Up</button>
+                <button type="submit">Sign Up</button>
         </form>
       </>
     )
