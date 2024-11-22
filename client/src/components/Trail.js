58758baaf7f903cde942f63b31df715ecd2864f3
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-22 15:55:21 -0800

    create Trail component

---
+++ client/src/components/Trail.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Trail({ name, address, length, description, profileImage }) {
+
+
+    return (
+        <div>
+            <img id="trail-pic" src={profileImage} alt="trail-pic" />
+            <h4 id="trail-name" >{name}</h4>
+            <address>{address}</address>
+            <p id="trail-length" >{length}</p>
+            <p id="trail-description" >{description}</p>
+        </div>
+    )
+}
+
+export default Trail
