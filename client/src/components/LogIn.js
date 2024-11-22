506befedc3a7a115852eb866f1ffe1fd3df8069c
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-22 14:05:03 -0800

    edit LogIn component to incorporate and pass OnLogin

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import LogInForm from "./LogInForm";
 import SignUpForm from "./SignUpForm";
 
-function LogIn() {
+function LogIn({ onLogin }) {
     const [hasAccount, setHasAccount] = useState(true)
 
     return (
         <div>
             {hasAccount ? (
                 <>
-                    <LogInForm />
+                    <LogInForm onLogin={onLogin} />
                     <br />
                     <p>Don't have an account?</p>
                     <button onClick={() => setHasAccount(false)}>Create An Account</button>
