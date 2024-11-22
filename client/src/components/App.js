2314bbda42642514bc5ca46a2391ec20701ae30f
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-22 14:08:11 -0800

    edit App component to create a state for user

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,11 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import LogIn from "./LogIn";
 
 function App() {
+  const [user, setUser] = useState(null)
+  console.log(`User: ${user}`)
+
   return (
     <div>
       <h1>Hiking Trails</h1>
       <h2>Welcome to Hiking Trails! With us you can search for hiking trails by cities!</h2>
-      <LogIn/>
+      <LogIn onLogin={setUser}/>
     </div>
   )
 }
