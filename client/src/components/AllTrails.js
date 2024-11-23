370e5978db8230ae52430c31bdd8d5f6b5605a02
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-22 22:31:54 -0800

    edit to include a search for city input

--- client/src/components/AllTrails.js
+++ client/src/components/AllTrails.js
@@ -1,27 +1,49 @@
-import React, { useEffect, useState } from "react";
-import Trail from "./Trail"
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import Trail from "./Trail";
 
 function AllTrails() {
     const [trails, setTrails] = useState([])
 
-    useEffect(() => {
-        // get trails
-        fetch("/trails")
-        .then(res => {
-          if (res.status === 200) {
-            res.json()
-            .then(resTrails => {
-              setTrails(resTrails)
-            })
-          }
-        })
-        .catch(error => console.error(error))
-      }, [])
+    const formik = useFormik({
+      initialValues : {
+          city: ""
+      },
+      onSubmit: (values) => {
+          fetch("trails", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(values, null, 2),
+          })
+          .then(res => {
+            if (res.status === 201) {
+              res.json()
+              .then(resTrails => {
+                setTrails(resTrails)
+              })
+            }
+          })
+          .catch(error => console.error(error))
+        },
+    })
 
     return (
         <div>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="search-city" >Enter City Here: </label>
+                <input 
+                  id="search-city"
+                  name="search-city"
+                  onChange={formik.handleChange}
+                  value={formik.values.city}
+                />
+                <button type="submit">Search</button>
+            </form>
             {/* Iterate through trails list and create a Trail JSX for each */}
-            {trails.map(trail => {
+            {trails.length > 0 ? (
+              trails.map(trail => {
                 return (
                   <Trail 
                     key={trail.id}
@@ -33,7 +55,9 @@ function AllTrails() {
                 />
                 )
             })
-            }
+            ) : (
+              <h4>Trails awaits you...</h4>
+            )}
         </div>
     )
 }
