ad84f457f7c0d40b58eed10b7c56db092149ab12
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-22 18:57:29 -0800

    edit App component to include All Trails component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import LogIn from "./LogIn";
+import AllTrails from "./AllTrails";
 
 function App() {
   const [user, setUser] = useState(null)
@@ -17,11 +18,21 @@ function App() {
     .catch(error => console.error(error))
   }, []);
 
+  if (!user) {
+    return (
+      <div>
+        <h1>Hiking Trails</h1>
+        <h2>Welcome to Hiking Trails! With us you can search for hiking trails by cities!</h2>
+        <LogIn onLogin={setUser}/>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Hiking Trails</h1>
       <h2>Welcome to Hiking Trails! With us you can search for hiking trails by cities!</h2>
-      <LogIn onLogin={setUser}/>
+      <AllTrails />
     </div>
   )
 }
