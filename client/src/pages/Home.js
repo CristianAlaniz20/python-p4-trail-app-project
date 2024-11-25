3e3a98c4202965f3a3a975a9ce6e988f3a8c35e7
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-25 14:13:09 -0800

    create Home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AllTrails from "../components/AllTrails";
+
+function Home() {
+    return (
+        <>
+            <AllTrails />
+        </>
+    )
+}
+
+export default Home
