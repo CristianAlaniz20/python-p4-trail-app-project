55846ec8aab2f9662b028eab58203ab2079fecda
Cristian Alaniz <alaniz21cris@gmail.com>
2024-11-25 14:15:18 -0800

    create Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <>
+            <h1>Hiking Trails</h1>
+            <h2>Welcome to Hiking Trails! With us you can search for hiking trails by cities!</h2>
+        </>
+    )
+}
+
+export default Header
