6e8698b34f9af6111d84fc9cd84d8fb6d59828f8
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-04 21:23:51 -0800

    add Save Trail button and function to handle click

--- client/src/components/Trail.js
+++ client/src/components/Trail.js
@@ -4,17 +4,38 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 function Trail({ id, name, address, length, description, profileImage }) {
     const history = useHistory()
 
-    function handleClick() {
+    function handleTrailDivClick() {
         history.push(`/trails/${id}`)
     }
 
+    function handleSaveTrailClick() {
+        fetch("/hiked_trails", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(id, null, 2),
+        })
+        .then(res => {
+            if (res.status === 200 || res.status === 201) {
+                res.json()
+                .then(data => {
+                    console.log(data)
+                    console.log("Trail sucessfully saved!")
+                })
+            }
+        })
+        .catch(error => console.error(error))
+    }
+
     return (
-        <div onClick={handleClick} >
+        <div onClick={handleTrailDivClick} >
             <img className="trail-pic" src={profileImage} alt={`${name} trail-pic`} />
             <h4 className="trail-name" >{name}</h4>
             <address className="trail-adress">{address}</address>
             <p className="trail-length" >{length} miles</p>
             <p className="trail-description" >{description}</p>
+            <button onClick={handleSaveTrailClick} >Save Trail</button>
         </div>
     )
 }
