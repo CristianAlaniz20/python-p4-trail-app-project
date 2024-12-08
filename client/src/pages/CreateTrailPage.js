50aca14e0f50a0c95e569c7478fedd74e6260740
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-08 02:16:29 -0800

    create CreateTrailPage

---
+++ client/src/pages/CreateTrailPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function CreateTrailPage() {
+    return (
+        <div>
+            <h2>Create Trail Page Form</h2>
+            <form>
+                <label>Trail Name:</label>
+                <input 
+                    
+                />
+                <br />
+
+                <label>Trail Address:</label>
+                <input 
+                
+                />
+                <br />
+
+                <label>Trail Length:</label>
+                <input 
+                
+                />
+                <br />
+
+                <label>Trail Description:</label>
+                <input 
+                
+                />
+                <br />
+
+                <label>Trail Image URL</label>
+                <input 
+                
+                />
+            </form>
+        </div>
+    )
+}
+
+export default CreateTrailPage
