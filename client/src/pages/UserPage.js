eade8e82e22cd11124d07b51de180bed98968398
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-07 19:11:13 -0800

    add Change Account Role button and functionality into UserPage

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import AllTrails from "../components/AllTrails";
+import ChangeRole from "../components/ChangeRole";
 
 function UserPage({ user }) {
     const [savedTrails, setSavedTrails] = useState([])
@@ -35,12 +36,20 @@ function UserPage({ user }) {
         .catch(error => console.error(error))
     }, [])
 
+    // Handle change account role button click
+    function handleChangeAccountRoleClick() {
+        return <ChangeRole />
+    }
+
     return (
         <div>
             <h1 id="user-username" >Username: {user.username}</h1>
             <img id="user-profile-pic" src={user.profile_image_url} alt={`${user.username} trail-pic`} />
             <p id="user-bio" >Bio: {user.bio}</p>
             <br />
+
+            <label htmlFor="change-role-button" >Role: {user.role}</label>
+            <button id="change-role-button" onClick={handleChangeAccountRoleClick} >Change Account Role</button>
 
             <h2>List of Saved Trails: </h2>
             <AllTrails trails={savedTrails} />
