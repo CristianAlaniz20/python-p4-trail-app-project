3d3318c9f3796ebc1dd7615b2a32aab7652be3a3
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-11 23:25:00 -0800

    remove SuccessfullyUpdated state and pass prop setUpdatedUser instead

--- client/src/pages/UpdateUserPage.js
+++ client/src/pages/UpdateUserPage.js
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import UpdateUser from "../components/UpdateUser";
 import PasswordConfirmation from "../components/PasswordConfirmation";
 
-function UpdateUserPage({ user }) {
+function UpdateUserPage({ user, setUpdateUser }) {
     const [passwordConfirmed, setPasswordConfirmed] = useState(false)
-    const [successfullyUpdated, setSuccessfullyUpated] = useState(false)
-    const history = useHistory()
-
-    if (successfullyUpdated) {
-        history.push(`/user`)
-    }
 
     return (
         <>
             <h2>Update User Page</h2>
             {passwordConfirmed ? 
-                <UpdateUser user={user} setSuccessfullyUpated={setSuccessfullyUpated} /> :
+                <UpdateUser user={user} setUpdateUser={setUpdateUser} /> :
                 <PasswordConfirmation user={user} setPasswordConfirmed={setPasswordConfirmed} />
             }
         </>
