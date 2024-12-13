4d0690c4b4000e838469af292cd7af41dd734704
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-12 19:55:08 -0800

    create userPassword state and pass setter function prop and variable prop to child components

--- client/src/pages/UpdateUserPage.js
+++ client/src/pages/UpdateUserPage.js
@@ -4,13 +4,14 @@ import PasswordConfirmation from "../components/PasswordConfirmation";
 
 function UpdateUserPage({ user, setUpdateUser }) {
     const [passwordConfirmed, setPasswordConfirmed] = useState(false)
+    const [userPassword, setUserPassword] = useState("")
 
     return (
         <>
             <h2>Update User Page</h2>
             {passwordConfirmed ? 
-                <UpdateUser user={user} setUpdateUser={setUpdateUser} /> :
-                <PasswordConfirmation user={user} setPasswordConfirmed={setPasswordConfirmed} />
+                <UpdateUser user={user} setUpdateUser={setUpdateUser} userPassword={userPassword} /> :
+                <PasswordConfirmation setPasswordConfirmed={setPasswordConfirmed} setUserPassword={setUserPassword} />
             }
         </>
     )
