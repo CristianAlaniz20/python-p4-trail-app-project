1345bc4b4a5f8d0b1773dd4b35307e4599426701
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-13 23:00:49 -0800

    create DeleteUserPage

---
+++ client/src/pages/DeleteUserPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function DeleteUserPage({ setDeleteAccount }) {
+    // handle YES button click
+    function handleYesClick() {
+        fetch("/delete_user", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+              },
+        })
+        .then(res => {
+            if (res.status == 204) {
+                console.log("Successfully deleted account!")
+            }
+        })
+        .catch(error => console.error(error))
+    }
+
+    // handle NO button click
+    const handleNoClick = () => setDeleteAccount(false)
+
+    return (
+        <div>
+            <p>
+                All account information will be lost.
+                Are you sure you want to delete your account?
+            </p>
+            <button onClick={handleYesClick} >YES</button>
+            <button onClick={handleNoClick} >NO</button>
+        </div>
+    )
+}
+
+export default DeleteUserPage
