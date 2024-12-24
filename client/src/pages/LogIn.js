15aad46078e0c1b4e1a187ad7ab6977f9d044b29
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-24 10:54:54 -0800

    LogIn.js: add comments and update to conditionally render ResponseMessage component

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import LogInForm from "../components/LogInForm";
 import SignUpForm from "../components/SignUpForm";
+import ResponseMessage from "../components/ResponseMessage";
+import { ResponseMessageContext }from "../components/ResponseMessageProvider";
 
 function LogIn({ onLogin }) {
     const [hasAccount, setHasAccount] = useState(true)
+    const {message, statusCode} = useContext(ResponseMessageContext)
+    console.log(`message: ${message} statusCode: ${statusCode}`)
 
     return (
         <div>
+            {/* conditionally shows Login form or Signup form */}
             {hasAccount ? (
                 <>
                     <LogInForm onLogin={onLogin} />
@@ -22,6 +27,8 @@ function LogIn({ onLogin }) {
                     <button onClick={() => setHasAccount(true)}>Go to Log In</button>
                 </>
             )}
+            {/* conditionally shows guiding message for users */}
+            {message && statusCode ? <ResponseMessage /> : null}
         </div>
     )
 }
