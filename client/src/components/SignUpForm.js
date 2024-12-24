e09db264a47fba1214e6e064c52463e7f57d8878
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-24 10:49:23 -0800

    SignUpForm.js: update to include handleResponse function

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import { ResponseMessageContext } from "./ResponseMessageProvider";
 
 function SignUpForm() {
+    const { handleResponse } = useContext(ResponseMessageContext)
+
     // formik validation schema
     const formSchema = yup.object().shape({
         username: yup.string().required("Must enter a username."),
@@ -26,11 +29,7 @@ function SignUpForm() {
               },
               body: JSON.stringify(values, null, 2),
             })
-            .then((res) => {
-              if (res.status === 201) {
-                console.log("Sucessfully signed up!")
-              }
-            })
+            .then((res) => handleResponse(res))
             .catch(error => console.error(error))
           },
         })
