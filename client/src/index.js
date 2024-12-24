f2dbdc31740e5c218045ed913d0a1cd2736d0fe0
Cristian Alaniz <alaniz21cris@gmail.com>
2024-12-24 10:36:57 -0800

    index.js: Wrap App with ResponseMessageProvider context component

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import ResponseMessageProvider from "./components/ResponseMessageProvider";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+    <ResponseMessageProvider>
+        <App />
+    </ResponseMessageProvider>
+);
