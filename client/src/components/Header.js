7d02f012b7aa291ba68c354afcd4f9d3014db773
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-02 20:46:22 -0800

    Header.js: add styling to component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Header() {
+    const headerStyle = {
+        textAlign: 'center',
+    }
+
     return (
-        <>
+        <div style={headerStyle}>
             <h1>Hiking Trails</h1>
             <h2>Welcome to Hiking Trails! With us you can search for hiking trails by cities!</h2>
-        </>
+        </div>
     )
 }
 
