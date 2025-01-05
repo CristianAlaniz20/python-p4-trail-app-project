eaafb37f861992dc2e045b7eeaf1bf6d9c19c516
Cristian Alaniz <alaniz21cris@gmail.com>
2025-01-04 18:26:23 -0800

    Review.js: add CSS styling

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Review({ rating, text, username }) {
+    const reviewStyle = {
+        border: "1px solid",
+    }
+
     return (
-        <div className="reviews" >
+        <div className="reviews" style={reviewStyle} >
             <h3 className="review-usernames" >{username}</h3>
             <p className="review-text" >{text}</p>
             <p className="review-rating" >{rating} out of 5</p>
